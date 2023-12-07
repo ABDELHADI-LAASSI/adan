45c1d9ed98084a16342f4a0de44535dee03748fd
abdelhadi <twitchabdelhadi@gmail.com>
2023-12-07 23:50:11 +0100

    fixing api error

--- script.js
+++ script.js
@@ -18,27 +18,50 @@ function adanTime() {
 
     let city = document.getElementById("city").value
 
-    axios.get(`http://api.aladhan.com/v1/calendarByCity?country=MA&&city=${city}`)
-    .then((response)=>{
-        let data = response.data
-        console.log(data)
+    fetch(`http://api.aladhan.com/v1/calendarByCity?country=MA&&city=${city}`)
+    .then((response) => response.json())
+    .then((json) => {
+        let data = json
+        console.log(json.data[1].date.gregorian.date)
         document.getElementById("show").innerHTML= ""
         for (ele in data.data) {
             document.getElementById("show").innerHTML+= `
             <div class="kharab">
-                    <p class="dayDate" >${response.data.data[ele].date.gregorian.date}</p>
+                    <p class="dayDate" >${json.data[ele].date.gregorian.date}</p>
                     <div class="timesContainer">
-                        <div> <p>  Fajr    </p> <p>${response.data.data[ele].timings.Fajr.split(' ')[0]}  </p>  </div> 
-                        <div> <p>  Choroq  </p> <p>${response.data.data[ele].timings.Sunrise.split(' ')[0]}  </p>  </div> 
-                        <div> <p>  Dohr    </p> <p>${response.data.data[ele].timings.Dhuhr.split(' ')[0]}  </p>  </div> 
-                        <div> <p>  Asr     </p> <p>${response.data.data[ele].timings.Asr.split(' ')[0]}  </p>  </div> 
-                        <div> <p>  Sunset  </p> <p>${response.data.data[ele].timings.Sunset.split(' ')[0]}  </p>  </div> 
-                        <div> <p>  Ichaa   </p> <p>${response.data.data[ele].timings.Maghrib.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Fajr    </p> <p>${json.data[ele].timings.Fajr.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Choroq  </p> <p>${json.data[ele].timings.Sunrise.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Dohr    </p> <p>${json.data[ele].timings.Dhuhr.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Asr     </p> <p>${json.data[ele].timings.Asr.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Sunset  </p> <p>${json.data[ele].timings.Sunset.split(' ')[0]}  </p>  </div> 
+                        <div> <p>  Ichaa   </p> <p>${json.data[ele].timings.Maghrib.split(' ')[0]}  </p>  </div> 
                     </div>
             </div>
             `
         }
-    })
+    });
+
+    // axios.get(`http://api.aladhan.com/v1/calendarByCity?country=MA&&city=${city}`)
+    // .then((response)=>{
+    //     let data = response.data
+    //     console.log(data)
+    //     document.getElementById("show").innerHTML= ""
+    //     for (ele in data.data) {
+    //         document.getElementById("show").innerHTML+= `
+    //         <div class="kharab">
+    //                 <p class="dayDate" >${response.data.data[ele].date.gregorian.date}</p>
+    //                 <div class="timesContainer">
+    //                     <div> <p>  Fajr    </p> <p>${response.data.data[ele].timings.Fajr.split(' ')[0]}  </p>  </div> 
+    //                     <div> <p>  Choroq  </p> <p>${response.data.data[ele].timings.Sunrise.split(' ')[0]}  </p>  </div> 
+    //                     <div> <p>  Dohr    </p> <p>${response.data.data[ele].timings.Dhuhr.split(' ')[0]}  </p>  </div> 
+    //                     <div> <p>  Asr     </p> <p>${response.data.data[ele].timings.Asr.split(' ')[0]}  </p>  </div> 
+    //                     <div> <p>  Sunset  </p> <p>${response.data.data[ele].timings.Sunset.split(' ')[0]}  </p>  </div> 
+    //                     <div> <p>  Ichaa   </p> <p>${response.data.data[ele].timings.Maghrib.split(' ')[0]}  </p>  </div> 
+    //                 </div>
+    //         </div>
+    //         `
+    //     }
+    // })
 }
 document.getElementById("btn").addEventListener("click",adanTime)
 
